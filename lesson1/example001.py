1561611801e58e5e7ee91ba6ece7860d2af6af38
stewstar <stevenwdiy2010@gmail.com>
2018-07-07 10:16:35 -0400

    update problem 1 solution
    run and test the solution for problem 1

--- lesson1/example001.py
+++ lesson1/example001.py
@@ -3,4 +3,12 @@
 Created on Sat Jul  7 09:52:17 2018
 
 """
+def solution():
+    result = []
+    for i in range(1000, 2001):
+        if (i % 5 == 0) and (i % 3 != 0):
+            result.append(str(i))
+    print(",".join(result))
 
+solution()
+            
